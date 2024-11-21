1d714359ebab689cc2b7e72aea4b3b5badc3bdb3
annisanurahma <annisanurahmah59@gmail.com>
2024-11-21 09:21:26 +0700

    car handler - Annisa

--- Assets/Scripts/Car/CarHandler.cs
+++ Assets/Scripts/Car/CarHandler.cs
@@ -6,8 +6,16 @@ public class CarHandler : MonoBehaviour
 {
     [SerializeField]
     Rigidbody rb;
+
+    [SerializeField]
+    Transform gameModel;
     // Start is called before the first frame update
    
+
+   //max values
+   float maxSteerVelocity = 2;
+   float maxForwardVelocity = 30;
+
     float accelerationMultiplier = 3;
     float breaksMultiplier = 15;
     float steeringMultiplier = 5;
@@ -22,7 +30,8 @@ public class CarHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Rotate car model when "turning"
+        gameModel.transform.rotation = Quaternion.Euler(0, rb.velocity.x * 5, 0);
     }
 
     private void FixedUpdate()
@@ -36,11 +45,19 @@ public class CarHandler : MonoBehaviour
             Brake();
 
         Steer();
+
+        //Force the car not to go backwards
+        if (rb.velocity.z <= 0)
+        rb.velocity = Vector3.zero;
     }
 
     void Accelerate()
     {
         rb.drag = 0;
+
+        //Stay within the speed limit
+        if (rb.velocity.z >= maxForwardVelocity)
+            return;
 
         rb.AddForce(rb.transform.forward * 10 * accelerationMultiplier * input.y);
     }
@@ -58,8 +75,26 @@ public class CarHandler : MonoBehaviour
     {
         if (Mathf.Abs(input.x) > 0)
         {
-            rb.AddForce(rb.transform.right * steeringMultiplier * input.x);
-        }
+            //Move the car sideways
+            float speedBaseSteerLimit = rb.velocity.z / 5.0f;
+            speedBaseSteerLimit = Mathf.Clamp01(speedBaseSteerLimit);
+
+            rb.AddForce(rb.transform.right * steeringMultiplier * input.x * speedBaseSteerLimit);
+
+            //Normalize the X Velocity
+            float normalizedX = rb.velocity.x /maxSteerVelocity;
+            
+            //Ensure that we dont allow it to get bigger than 1 in magnitued
+            normalizedX = Mathf.Clamp(normalizedX, -1.0f, 1.0f) ;
+
+            //Make sure we stay within the turn speed limit
+            rb.velocity = new Vector3(normalizedX * maxSteerVelocity, 0, rb.velocity.z);  
+            }
+            else
+            {
+                //Auto center car
+                rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(0, 0, rb.velocity.z), Time.fixedDeltaTime * 3);
+            }
     }
 
     public void SetInput(Vector2 inputVector)
